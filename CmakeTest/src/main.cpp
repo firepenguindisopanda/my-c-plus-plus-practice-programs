c438532a1ef9104a4415fc4a0b616d2bc2a1d638
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-28 17:39:52 -0400

    Updated: checks for args

--- CmakeTest/src/main.cpp
+++ CmakeTest/src/main.cpp
@@ -2,6 +2,15 @@
 #include <bits/stdc++.h>
 
 int main(int argc, char** argv){
+    if(argc < 2){
+        std::cout << "Usage: " << argv[0] << " <name>" << std::endl;
+    }
+    if(argv[1] != NULL){
+        std::cout << "Hello " << argv[1] << std::endl;
+    }
+    else{
+        std::cout << "Hello World" << std::endl;
+    }
     std::cout <<"Testin World" << std::endl;
     return 0;
 }
