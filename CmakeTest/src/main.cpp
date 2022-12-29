e2858643335d563938eec5faead687813f6144af
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-29 15:05:48 -0400

    Added: weird algorithm function for practice

--- CmakeTest/src/main.cpp
+++ CmakeTest/src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
+void weirdAlgorithm(long long n){
+    std::cout << n << " ";
+    if(n == 1){
+        return;
+    }
+    if(n % 2 == 0){
+        weirdAlgorithm(n/2);
+    }
+    else{
+        weirdAlgorithm(3*n + 1);
+    }
+}
+
 int main(int argc, char** argv){
     if(argc < 2){
         std::cout << "Usage: " << argv[0] << " <name>" << std::endl;
@@ -12,5 +25,9 @@ int main(int argc, char** argv){
         std::cout << "Hello World" << std::endl;
     }
     std::cout <<"Testin World" << std::endl;
+    long long n;
+    n = 3;
+    weirdAlgorithm(n);
+    system("pause");
     return 0;
 }
