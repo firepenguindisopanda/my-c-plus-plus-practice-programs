dd3142ec5c8c1a6492e2a51a424925fdd9665b8d
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-29 20:38:24 -0400

    Added: a struct named Condition

--- HospitalBytes/DataTypes.h
+++ HospitalBytes/DataTypes.h
@@ -1,7 +1,12 @@
 #ifndef _DATATYPES_H
 #define _DATATYPES_H
+#include <iostream>
+using namespace std;
 
-
-
+// A STRUCT CONTAINING attributes for a Condition object
+struct Condition {
+    string name;
+    int priority;
+};
 
 #endif
