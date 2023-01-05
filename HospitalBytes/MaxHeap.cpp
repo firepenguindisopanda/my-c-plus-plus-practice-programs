87779a96fe0d9fb6b36004351fd41c37145cab17
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-04 22:18:40 -0400

    Updated: with definition for maxHeapify fn

--- HospitalBytes/MaxHeap.cpp
+++ HospitalBytes/MaxHeap.cpp
@@ -17,3 +17,31 @@ int rightChild(int i)
 {
     return (2*i) + 1;
 }
+
+/**
+ * @brief maxHeapify
+ * Assuming that the left and right subtrees
+ * of node i are max heaps, this function
+ * maintains the max heap property starting
+ * at node i.
+ * 
+ */
+void maxHeapify(Incident A[], int heapSize, int i)
+{
+    int l = leftChild(i);
+    int r = rightChild(i);
+    int largest = i;
+    if (l <= heapSize && A[l].priority > A[i].priority)
+        largest = l;
+        
+    if (r <= heapSize && A[r].priority > A[largest].priority)
+        largest = r;
+
+    if (largest != i)
+    {
+        Incident temp = A[i];
+        A[i] = A[largest];
+        A[largest] = temp;
+        maxHeapify(A, heapSize, largest);
+    }
+}
