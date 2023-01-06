4fd02d8768f829472db31064fd8e50c7484ef7b9
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-05 23:31:26 -0400

    Updated: with the definition for maxHeapInsert fn

--- HospitalBytes/MaxPriorityQueue.cpp
+++ HospitalBytes/MaxPriorityQueue.cpp
@@ -3,3 +3,19 @@
 #include "MaxHeap.h"
 #include "MaxPriorityQueue.h"
 using namespace std;
+
+/**
+ * @brief Inserts a new incident in the max priority queue
+ * stored in the array A based on its priority.
+ * @param {Incident A[]} The array of type Incident
+ * @param {int heapSize} The size of the heap
+ * @param {Incident newIncident} The new incident to be inserted
+ */
+void maxHeapInsert(Incident A[], int heapSize, Incident newIncident)
+{
+    heapSize++;
+    A[heapSize].patientID = newIncident.patientID;
+    A[heapSize].conditionName = newIncident.conditionName;
+    A[heapSize].priority = INT_MIN;
+    heapIncreaseKey(A, heapSize, newIncident.priority);
+}
