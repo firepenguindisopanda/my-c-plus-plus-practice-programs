17757ee69f5aaf7fbed2c838cb6fc68fa17103d5
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-05 23:41:45 -0400

    Updated: heapIncreaseKey fn definition

--- HospitalBytes/MaxPriorityQueue.cpp
+++ HospitalBytes/MaxPriorityQueue.cpp
@@ -35,3 +35,47 @@ Incident heapMaximum(Incident A[])
     highestIncident.priority = A[1].priority;
     return highestIncident;
 }
+
+/**
+ * @brief Removes and returns the highest priority element in the max priority queue
+ * @param {Incident A[]} The array of type Incident
+ * @param {int heapSize} The size of the heap
+ * @return {Incident} The highest priority element in the max priority queue
+ */
+Incident heapExtractMax(Incident A[], int heapSize)
+{
+    if (heapSize < 1)
+    {
+        cout << "Heap underflow" << endl;
+        return A[0];
+    }
+    Incident max = A[1];
+    A[1] = A[heapSize];
+    heapSize--;
+    maxHeapify(A, heapSize, 1);
+    return max;
+}
+
+/**
+ * @brief Increases the value of the element i's priority to the new value,
+ * newPriority, where newPriority is greater than or equal to i's current priority.
+ * @param {Incident A[]} The array of type Incident
+ * @param {int i} The index of the element whose priority is to be increased
+ * @param {int newPriority} The new priority of the element
+ */
+void heapIncreaseKey(Incident A[], int i, int newPriority)
+{
+    if (newPriority < A[i].priority)
+    {
+        cout << "New priority is smaller than current priority" << endl;
+        return;
+    }
+    A[i].priority = newPriority;
+    while (i > 1 && A[parent(i)].priority < A[i].priority)
+    {
+        Incident temp = A[i];
+        A[i] = A[parent(i)];
+        A[parent(i)] = temp;
+        i = parent(i);
+    }
+}
