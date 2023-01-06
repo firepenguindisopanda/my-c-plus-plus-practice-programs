7de8e19733736d01bbddbb7082d83dcab4d9ca1c
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-05 23:55:22 -0400

    Added: file with definition for fn from header file of same name

---
+++ HospitalBytes/Queue.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+#include "DataTypes.h"
+#include "Queue.h"
+
+using namespace std;
+
+
+Queue * initQueue () {
+	Queue * q = new Queue;
+	q->top = NULL;
+	q->last = NULL;
+	return q;
+}
+
+
+bool isEmptyQueue (Queue * q) {
+	if (q->top == NULL)
+		return true;
+	else
+		return false;		
+}
+
+
+int sizeQueue (Queue * q) {
+
+	QueueNode * curr;
+	int size;
+
+	curr = q->top;	
+	size = 0;
+	
+	while (curr != NULL) {
+		curr = curr->next;
+		size = size + 1;
+	}
+	
+	return size;
+}
+
+void enqueue (Queue * q, Incident data) {
+	QueueNode * newElement = new QueueNode;
+	newElement->incidentData = data;
+	newElement->next = NULL;
+	
+	if (isEmptyQueue(q)) {
+		q->top = newElement;
+		q->last = newElement;
+	}
+	else {
+		q->last->next = newElement;
+		q->last = newElement;
+	}
+}
+
+
+Incident dequeue (Queue * q) {
+
+	QueueNode * toRemove;
+	Incident data;
+
+	if (isEmptyQueue(q)) {
+		data.patientID = -1;		// to indicate error if dequeuing from an empty queue
+		return data;
+	}
+	
+	toRemove = q->top;
+	data = toRemove->incidentData;
+	q->top = toRemove->next;
+	if (q->top == NULL)
+		q->last = NULL;
+		
+	delete toRemove;
+	
+	return data;
+}
