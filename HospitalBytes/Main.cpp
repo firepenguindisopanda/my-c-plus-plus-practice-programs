35e22ac2f53f4ab7aa8c771039cd36e5c4e2bca2
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-08 01:33:17 -0400

    Updated: with logic to read incident text file

--- HospitalBytes/Main.cpp
+++ HospitalBytes/Main.cpp
@@ -19,6 +19,7 @@ int main()
 {
     // read patient data and save to binary search tree
     ifstream patientData;
+    ifstream incidentData;
     patientData.open("Patient.txt");
     if(!patientData)
     {
@@ -26,7 +27,13 @@ int main()
         exit(1);
     }
     int patientID{};
+    int incidentId{}, priority{0}, heapSize{0}, count{0};
     string patientName, patientPhoneNumber;
+    string condition;
+
+    Incident newIncident;
+    Incident incidentArray[1000];
+
     patientData >> patientID;
     Patient patientInfo;
     BTNode * root = nullptr;
@@ -52,10 +59,42 @@ int main()
     cout << "\nI'm done reading the patient data" << endl;
 
     // Displaying the binary search tree
-    cout << "\nDisplaying the binary search tree" << endl;
-    displayBST(root);
+    // cout << "\nDisplaying the binary search tree" << endl;
+    // displayBST(root);
     // displayPreOrder(root);
     // displayPostOrder(root);
 
+    incidentData.open("Incident.txt");
+    if(!incidentData.is_open()){
+        cout << "Could not find Incident File!..." << endl;
+        exit(1);
+    }
+    // setup the incident array with initial values at 0 or " "
+    for(count = 0; count < 1000; count++)
+    {
+        incidentArray[count].patientID = 0;
+        incidentArray[count].conditionName = " ";
+        incidentArray[count].priority = 0;
+    }
+    incidentData >> incidentId;
+    while(incidentId != 999){
+        newIncident.patientID = incidentId;
+        incidentData >> condition;
+        newIncident.conditionName = condition;
+        incidentData >> priority;
+        newIncident.priority = priority;
+        heapSize++;
+        maxHeapInsert(incidentArray, heapSize, newIncident);
+        incidentData >> incidentId;
+    }
+
+    // print the incident array
+    cout << "\nDisplaying the incident array" << endl;
+    for(count = 1; count <= heapSize; count++)
+    {
+        cout << incidentArray[count].patientID << " " << incidentArray[count].conditionName << " " << incidentArray[count].priority << endl;
+    }
+
     system("pause");
+    return 0;
 }
